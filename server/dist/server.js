3bd0978788ced873e4e3d3bd31229f0291829008
archanpatel1425 <archanpatel6070@gmail.com>
2025-01-01 11:17:12 +0530

    change in apollo server code in server.js

--- server/dist/server.js
+++ server/dist/server.js
@@ -3,17 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
 const client_1 = require("@prisma/client");
-const apollo_server_core_1 = require("apollo-server-core");
 const apollo_server_express_1 = require("apollo-server-express");
-const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
-const graphql_1 = require("graphql");
 const index_1 = __importDefault(require("./GraphQL/resolvers/index"));
 const typeDefs_1 = __importDefault(require("./GraphQL/typeDefs"));
+require("dotenv").config();
 const app = (0, express_1.default)();
 const prisma = new client_1.PrismaClient();
 const corsOptions = {
@@ -22,34 +19,23 @@ const corsOptions = {
 };
 app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
-app.use(body_parser_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.static("public"));
-const main = async () => {
-    const apolloServer = new apollo_server_express_1.ApolloServer({
-        typeDefs: typeDefs_1.default,
-        resolvers: index_1.default,
-        cache: 'bounded',
-        plugins: [(0, apollo_server_core_1.ApolloServerPluginLandingPageGraphQLPlayground)()],
-        introspection: true,
-        formatError: (error) => {
-            if (error.originalError) {
-                return {
-                    message: error.message,
-                    code: error.extensions.code,
-                };
-            }
-            return (0, graphql_1.formatError)(error);
-        },
-    });
-    await prisma.$connect();
-    await apolloServer.start();
-    apolloServer.applyMiddleware({
-        app: app,
-        cors: false,
-        bodyParserConfig: { limit: "1tb" },
-    });
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => { console.log('server running on port 5000'); });
+const apolloServer = new apollo_server_express_1.ApolloServer({
+    typeDefs: typeDefs_1.default,
+    resolvers: index_1.default,
+    cache: 'bounded',
+});
+const startServers = async () => {
+    try {
+        await prisma.$connect();
+        console.log('databse connected');
+        await apolloServer.start();
+        apolloServer.applyMiddleware({ app: app, cors: false });
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => { console.log('server running on port 5000'); });
+    }
+    catch (err) {
+        console.error('Error starting servers:', err);
+    }
 };
-main().catch((err) => console.error(err));
+startServers();
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,60 +1,39 @@
-require("dotenv").config();
 import { PrismaClient } from '@prisma/client';
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-express";
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
-import express, { Application } from "express";
-import { DocumentNode, formatError } from "graphql";
-import { JwtPayload } from 'jsonwebtoken';
+import express, { Express } from "express";
 import resolvers from './GraphQL/resolvers/index';
 import typeDefs from './GraphQL/typeDefs';
 
-const app: Application = express();
+require("dotenv").config();
+
+
+const app: Express = express();
 const prisma = new PrismaClient();
 const corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true,
 };
-
 app.use(cors(corsOptions));
 app.use(express.json())
-app.use(bodyParser.json())
 app.use(cookieParser());
-app.use(express.static("public"));
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cache: 'bounded',
+});
 
-
-const main = async () => {
-  type DecodedToken = JwtPayload & { userId: string };
-
-  const apolloServer = new ApolloServer({
-    typeDefs: typeDefs as DocumentNode,
-    resolvers,
-    cache: 'bounded',
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-    introspection: true,
-    formatError: (error) => {
-      if (error.originalError) {
-        return {
-          message: error.message,
-          code: error.extensions.code,
-        };
-      }
-      return formatError(error);
-    },
-  });
-  await prisma.$connect();
-  await apolloServer.start();
-  apolloServer.applyMiddleware({
-    app: app as any,
-    cors: false,
-    bodyParserConfig: { limit: "1tb" },
-
-  });
-
-  const PORT = process.env.PORT || 5000
-
-  app.listen(PORT, () => { console.log('server running on port 5000') });
-};
-main().catch((err) => console.error(err));
+const startServers = async () => {
+  try {
+    await prisma.$connect();
+    console.log('databse connected');
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app: app as any, cors: false });
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => { console.log('server running on port 5000') });
+  } catch (err) {
+    console.error('Error starting servers:', err);
+  }
+}
+startServers();
